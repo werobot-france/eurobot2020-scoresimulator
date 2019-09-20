ee8590832f0f1750f61057ea2bb37ec86e27b3ad
Matthieu Bessat <spamfree@matthieubessat.fr>
2019-09-20 21:30:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Switch, Container, Typography, Divider, TextField, Grid, FormControl, Paper, CardContent, Card } from '@material-ui/core'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <Container>
+          <div className="header">
+            <div className="header-content">
+              <div class="header-logos">
+                <img src="http://www.eurobot.org/media/eurobot.png" className="eurobot-logo" style={{width: '10em'}} />
+                <img src="https://s.werobot.fr/logo.png" className="werobot-logo" style={{width: '10em'}} />
+              </div>
+              <div class="header-title">
+                <Typography variant="h4">
+                  Eurobot 2020 score simulation
+                </Typography>
+                <Typography variant="h6">
+                  Compute your game score online!
+                </Typography>
+                <Typography>
+                  Created by the <a href="https://werobot.fr">We Robot</a> team to help you!
+                </Typography>
+              </div>
+              {/*
+              <img src="https://s.werobot.fr/logo.png" style={{width: '10em'}} /> 
+              */}
+            </div>
+          </div>
+          <Divider />
+          <div class="form">
+            <Grid container>
+              <Grid item>
+                <TextField
+                  fullWidth="true"
+                  label="Votre Estimation"
+                  type="number"
+                />
+              </Grid>
+              <Grid item>
+                <Card>
+                  <CardContent>
+                    <Typography component="h5" variant="h5">
+                      125
+                    </Typography>
+                    <Typography>
+                      Points
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </div>
+          <Divider />
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
