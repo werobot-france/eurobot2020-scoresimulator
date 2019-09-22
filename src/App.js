8933ecc9d7cefacfa07e2ecec88b06fe16c650e8
Matthieu Bessat <spamfree@matthieubessat.fr>
2019-09-22 13:07:41 +0200

    add i18n & score compute

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ import {
   FormControlLabel,
   FormLabel,
   Checkbox,
-  FormGroup
+  FormGroup,
+  ButtonGroup,
+  Button
 } from '@material-ui/core'
 import './style.css'
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import { indigo } from '@material-ui/core/colors';
+import { indigo, purple } from '@material-ui/core/colors';
+import { withTranslation } from 'react-i18next';
 
 const theme = createMuiTheme({
   palette: {
@@ -27,28 +30,37 @@ const theme = createMuiTheme({
     },
     primary: {
       main: indigo[700],
+    },
+    accent: {
+      main: purple[500]
     }
   },
 });
 
-
-export default class App extends React.Component {
+const App = class App extends React.Component {
 
   state = {
-    totalScore: 0,
+    score: 0,
+    totalScore: 20,
+    bonus: 0,
+    lighthouseCanBeDeployed: false,
+    lighthouseCanBeEnabled: false
+  }
+
+  inputs = {
     // buoys
     buoysInPort: 0,
     buoysInColoredChannel: 0,
     buoysValidPairs: 0,
     // lighthouse
+    lighthouseExists: false,
     lighthouseEnabled: false,
     lighthouseDeployed: false,
-    lighthouseExists: false,
     // windsock
-    windsocks: 0,
-    orientation: '',
+    windsocks: 'none',
+    orientation: 'none',
     flags: '',
-    estimate: 0,
+    estimate: 0
   }
 
   computeScore = (event) => {
@@ -56,18 +68,63 @@ export default class App extends React.Component {
     let targetValue = event.target.type !== 'checkbox' ? event.target.value : event.target.checked
     if (targetType === 'number') {
       targetValue = parseInt(targetValue)
+      if (isNaN(targetValue) || targetValue < 0) {
+        targetValue = 0
+      }
     }
     console.log(targetType, event.target.name, targetValue) 
     
-    let state = {}
-    state[event.target.name] = targetValue
-    this.setState(state)
+    let inputs = this.inputs
+    inputs[event.target.name] = targetValue
+    this.inputs = inputs
+    //console.log(inputs)
+
+    let lighthouseCanBeEnabled = inputs.lighthouseExists
+    let lighthouseCanBeDeployed = inputs.lighthouseEnabled
+
+    let totalScore = 0;
+    totalScore += inputs.buoysInPort;
+    totalScore += inputs.buoysInColoredChannel;
+    totalScore += inputs.buoysValidPairs * 2;
+    if (inputs.windsocks === 'one') {
+      totalScore += 5
+    }
+    if (inputs.windsocks === 'both') {
+      totalScore += 15
+    }
+    if (inputs.lighthouseExists) {
+      totalScore += 2
+    }
+    if (inputs.lighthouseExists && inputs.lighthouseEnabled) {
+      totalScore += 3
+    }
+    if (inputs.lighthouseExists && inputs.lighthouseDeployed && inputs.lighthouseEnabled) {
+      totalScore += 10
+    }
+    if (inputs.orientation === 'bad') {
+      totalScore += 5
+    }
+    if (inputs.orientation === 'good') {
+      totalScore += 10
+    }
+    if (inputs.flags === 'deployed') {
+      totalScore += 10
+    }
+    let score = totalScore;
+    let bonus =  (0.30 * totalScore).toFixed(0) - (Math.abs(totalScore - inputs.estimate));
+    bonus =  (bonus < 0 ? 0 : bonus);
+    totalScore += bonus;
     
-    this.setState({totalScore: Math.random()})
+    this.setState({score, bonus, totalScore, lighthouseCanBeDeployed, lighthouseCanBeEnabled})
     
+  }
+
+  switchLocale = (locale) => {
+    this.props.i18n.changeLanguage(locale)
   }
   
   render() {
+    const { t, i18n } = this.props;
     return (
       <div>
         <ThemeProvider theme={theme}>
@@ -75,19 +132,44 @@ export default class App extends React.Component {
           <div className="header">
             <div className="header-content">
               <div className="header-logos">
-                <img src="http://www.eurobot.org/media/eurobot.png" className="eurobot-logo" style={{width: '10em'}} />
-                <img src="https://s.werobot.fr/logo.png" className="werobot-logo" style={{width: '10em'}} />
+                <img 
+                  src="http://www.eurobot.org/media/eurobot.png"
+                  alt="Eurobot's logo"
+                  className="eurobot-logo"
+                  style={{width: '10em'}} />
+                <img
+                  src="https://s.werobot.fr/logo.png"
+                  alt="We Robot's logo"
+                  className="werobot-logo"
+                  style={{width: '10em'}} />
               </div>
               <div className="header-title">
                 <Typography variant="h4">
-                  Eurobot 2020 score simulation
+                  {t('header.title')}
                 </Typography>
-                <Typography variant="h6">
-                  Compute your game score online!
-                </Typography>
-                <Typography>
-                  Created by the <a href="https://werobot.fr">We Robot</a> team to help you!
-                </Typography>
+                <div className="header-sub">
+                  <div>
+                    <Typography variant="h6">
+                      {t('header.description')}
+                    </Typography>
+                    <Typography dangerouslySetInnerHTML={({__html: t('header.author', { werobot: '<a href="https://werobot.fr">We Robot</a>' })})}>
+                    </Typography>
+                  </div>
+                  <div className="header-locales">
+                    <ButtonGroup size="small" color="primary">
+                      <Button 
+                        onClick={() => this.switchLocale('fr')}
+                        disabled={i18n.language === 'fr'}>
+                          Français
+                      </Button>
+                      <Button
+                        onClick={() => this.switchLocale('en')}
+                        disabled={i18n.language === 'en'}>
+                          English
+                      </Button>
+                    </ButtonGroup>
+                  </div>
+                </div>
               </div>
               {/*
               <img src="https://s.werobot.fr/logo.png" style={{width: '10em'}} /> 
@@ -96,8 +178,8 @@ export default class App extends React.Component {
           </div>
           <Divider style={{marginBottom: '1em', marginTop: '1em'}} />
           <div className="form">
-            <Grid container>
-              <Grid item xs={12} md={8} style={{marginBottom: "1em"}}>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={8}>
                 <Grid container spacing={3}>
                   <Grid item xs={12} md={8} lg={6}>
                     <FormControl component="fieldset" fullWidth>
@@ -131,7 +213,7 @@ export default class App extends React.Component {
                   <Grid item>
                     <FormControl component="fieldset">
                       <FormLabel component="legend">Manches à air levés</FormLabel>
-                      <RadioGroup name="windsocks" value="none">
+                      <RadioGroup name="windsocks" value={this.inputs.windsocks}>
                         <FormControlLabel 
                           onChange={this.computeScore}
                           control={<Radio />}
@@ -160,15 +242,18 @@ export default class App extends React.Component {
                           onChange={this.computeScore}
                         />
                         <FormControlLabel
-                          control={<Checkbox name="lighthouseEnabled" />}
+                          control={<Checkbox 
+                            disabled={!this.state.lighthouseCanBeEnabled}
+                            name="lighthouseEnabled" />}
                           label="Phare allumé (3pts)"
                           onChange={this.computeScore}
                         />
                         <FormControlLabel
-                          control={<Checkbox name="lighthouseDeployed" />}
+                          control={<Checkbox 
+                            disabled={!this.state.lighthouseCanBeDeployed || !this.state.lighthouseCanBeEnabled}
+                            name="lighthouseDeployed" />}
                           label="Phare déployé (et allumé) (10pts)"
                           onChange={this.computeScore}
-                          disabled={this.state.lightH}
                         />
                       </FormGroup>
                     </FormControl>
@@ -178,7 +263,7 @@ export default class App extends React.Component {
                       <FormLabel component="legend" color="red">
                         Arriver à bon port
                       </FormLabel>
-                      <RadioGroup name="orientation" value="none">
+                      <RadioGroup name="orientation" value={this.inputs.orientation}>
                         <FormControlLabel 
                           value="none"
                           control={<Radio />}
@@ -202,7 +287,7 @@ export default class App extends React.Component {
                       <FormLabel component="legend">
                         Pavillons
                       </FormLabel>
-                      <RadioGroup name="flags" value="none">
+                      <RadioGroup name="flags" value={this.inputs.flags}>
                         <FormControlLabel
                           value="none"
                           control={<Radio />}
@@ -232,16 +317,44 @@ export default class App extends React.Component {
                 </Grid>
               </Grid>
               <Grid item xs={12} md={4}>
-                <Card>
-                  <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                      Total score
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                      {this.state.totalScore}
-                    </Typography>
-                  </CardContent>
-                </Card>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Card>
+                      <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                          Score (sans bonus)
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                          {this.state.score}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Card>
+                      <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                          Bonus
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                          {this.state.bonus}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Card>
+                      <CardContent>
+                        <Typography color="secondary" gutterBottom>
+                          Score total
+                        </Typography>
+                        <Typography variant="h4" component="h2">
+                          {this.state.totalScore}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </div>
@@ -252,3 +365,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default withTranslation()(App)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './style.scss'
+import './i18n';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
